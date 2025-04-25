53c804e1bc6dff254cb69f77402c6fc84562dcfe
Gautam Ramnath <mayaramp@hotmail.com>
2025-04-25 20:47:09 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,11 @@
+prijzen = {
+    "aardbei": 3,
+    "vanille": 4,
+    "chocolade": 5
+}
+# Stap 2: Variabele 'aanbieding' berekenen
+aanbieding = prijzen["aardbei"] * 0.8
+
+# Stap 3: Formatteer de reclame-tekst
+reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter slechts € {aanbieding}"
+print(reclame_tekst)  # Eerste print-opdracht
