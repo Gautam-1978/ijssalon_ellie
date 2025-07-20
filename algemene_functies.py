d814ce7a6022e56faca9a69c8ea1fe6ec0606537
Gautam Ramnath <mayaramp@hotmail.com>
2025-07-20 23:09:03 +0200

    dit is algemene functies

---
+++ algemene_functies.py
@@ -0,0 +1,11 @@
+def mijn_functie_1(x):
+    return x * x
+
+def mijn_functie_2(x, y):
+    return [
+        x + y,
+        x - y,
+        x * y,
+        x // y   # let op: dit is een hele deling
+     ]
+
--- tijdelijk.py
+++ tijdelijk.py
@@ -1,27 +1,27 @@
-prijzen = {
-    "aardbei": 3,
-    "vanille": 4,
-    "chocolade": 5
-}
-# Stap 2: Variabele 'aanbieding' berekenen
-aanbieding = prijzen["aardbei"] * 0.8
+# Dictionary met de prijzen van verschillende smaken ijs
+prijzen = {"aardbei" : 3,      # Prijs van aardbei-ijs is €3
+           "vanille" : 4,      # Prijs van vanille-ijs is €4
+           "chocolade" : 5     # Prijs van chocolade-ijs is €5
+ }
 
-# Stap 3: Formatteer de reclame-tekst
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter slechts € {aanbieding}"
+# Berekening van de aanbieding: aardbei-ijs met 20% korting
+aanbieding = prijzen["aardbei"] * 0,8 # 3 * 0,8 = 2,40
 
-print(reclame_tekst)  # Eerste print-opdracht
-reclame_tekst2 = reclame_tekst[:reclame_tekst.index('0')]
+# Opstellen van een reclamebericht met de aanbieding
+reclame_tekst = f"Vandaag in de aanbieding: aarbei-ijs, 1 liter - slechts € {aanbieding}"
 
-print(reclame_tekst2)
+# Beperkt de lengte van de tekst tot de eerste 63 tekens
+reclame_tekst2 = reclame_tekst[:63]
+
+# Zet de tekst om naar hoofdletters
 reclame_tekst3 = reclame_tekst2.upper()
 
-print(reclame_tekst3)
-reclame_tekst4 = reclame_tekst3.split()
 
-print(reclame_tekst4)
 
-for el in reclame_tekst4:
-    if len(el) >= 5:
-        print(el.upper())
+# Loopt door elk woord in de lijst
+for el in reclame_tekst3:
+    # Controleert of het woord langer is dan 4 tekens
+    if len(el) > 4:
+        print(el.upper())  # Print het woord in hoofdletters 
     else:
-        print(el.lower())
+        print(el.lower())  # Print het woord in kleine letters
